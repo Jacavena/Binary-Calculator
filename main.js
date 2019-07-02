67a03444b173031ef47833abb5c31111eac4d1b4
Jacavena <jcvnacua_0777@yahoo.com>
2019-07-03 00:16:54 +0800

    Add Binary Addition/Subtraction functionality

--- main.js
+++ main.js
@@ -1,49 +1,126 @@
+let baseBinary1 = document.getElementById("baseBinary1");
+let baseBinary2 = document.getElementById("baseBinary2");
+let baseDecimal1 = document.getElementById("baseDecimal1");
+let baseDecimal2 = document.getElementById("baseDecimal2");
 
-let concatBin = () => {
+let concatBin = () => { // Concatenate Binary
   //Input 1
   let binary1 = ""
   let dir1 = document.querySelectorAll('.binaryInput1 button');
   for(let i = 0; i < dir1.length; i++) {
     binary1 += dir1[i].textContent;
   }
-  document.getElementById("baseBinary1").innerText = binary1;
+  baseBinary1.textContent = binary1;
   //Input 2
   let binary2 = ""
   let dir2 = document.querySelectorAll('.binaryInput2 button');
   for(let i = 0; i < dir2.length; i++) {
     binary2 += dir2[i].textContent;
   }
-  document.getElementById("baseBinary2").innerText = binary2;
+  baseBinary2.textContent = binary2;
+  bin2Dec(); // Convert to Decimal
+  addBin(); // Perform Addition
 }
 
-window.onload = concatBin();
-
-let bin2Dec = () => {
+let bin2Dec = () => { // Binary to Decimal Conversion
   // Input 1
-  let binary1 = document.getElementById("baseBinary1").innerText;
+  let binary1 = baseBinary1.textContent;
   let arr1 = binary1.split('');
   let decimal1 = 0;
   for(let i in arr1) {
     decimal1 += Number(arr1[i]*(2**(arr1.length-i-1)))
   }
-  document.getElementById("baseDecimal1").innerText = decimal1;
+  baseDecimal1.textContent = decimal1;
   // Input 2
-  let binary2 = document.getElementById("baseBinary2").innerText;
+  let binary2 = baseBinary2.textContent;
   let arr2 = binary2.split('');
   let decimal2 = 0;
   for(let i in arr2) {
     decimal2 += Number(arr2[i]*(2**(arr2.length-i-1)))
   }
-  document.getElementById("baseDecimal2").innerText = decimal2;
+  baseDecimal2.innerText = decimal2;
+  opDec('add'); // Perform Addition
+  opDec('sub'); // Perform Subtraction
+  opDec('mult'); // Perform Multiplication
+  opDec('div'); // Perform Division
 }
-window.onload = bin2Dec
+
+let opDec = (operator) => {
+  let baseDec1 = operator == 'div' ? parseFloat(baseDecimal1.textContent): 
+    Number(baseDecimal1.textContent);
+  let baseDec2 = operator == 'div' ? parseFloat(baseDecimal2.textContent):
+    Number(baseDecimal2.textContent);
+  let ansDec = operator == 'add'? baseDec1 + baseDec2: operator == 'sub' ?
+    baseDec1 - baseDec2: operator == 'mult' ? baseDec1 * baseDec2:
+    Math.round((baseDec1 / baseDec2) * 100) / 100;
+  let id = operator == 'add' ? 'addDecimal': operator == 'sub' ? 'subDecimal':
+    operator == 'mult' ? 'multDecimal': 'divDecimal';
+  document.getElementById(id).textContent = ansDec;
+}
 
 let addBin = () => {
+  let binary1 = baseBinary1.textContent;
+  let binary2 = baseBinary2.textContent;
+  let arrBin1 = binary1.split('');
+  arrBin1.unshift('0');
+  let arrBin2 = binary2.split('');
+  arrBin2.unshift('0');
+  let sumBin = [];
+  for (let i = arrBin1.length-1; i >=0; i--) {
+    let ele1 = Number(arrBin1[i]), ele2 = Number(arrBin2[i]);
+    let sum = ele1 + ele2;
+    if(sum == 2) {arrBin1[i-1] = Number(arrBin1[i-1]) + 1; sum = 0;}
+    if(sum == 3) {arrBin1[i-1] = Number(arrBin1[i-1]) + 1; sum = 1;}
+    sumBin.unshift(sum);
+  }
+  document.getElementById("addBinary").textContent = sumBin.join('');
+  subBin(); // Subtract Binary
+}
+
+let subBin = () => {
+  let bin1 = baseBinary1.textContent.split('');
+  let bin2 = baseBinary2.textContent.split('');
+  let check = checkHigher(bin1, bin2);
+  let diffBin = [];
+  if(check == 'equal') {
+    for (let i in bin1) {
+      diffBin.push('0');
+    }
+  } else if(check == 'arr1') diffBin = subOperation(bin1, bin2);
+  else if(check == 'arr2') {
+    diffBin = subOperation(bin2, bin1);
+    diffBin.unshift('-');
+  }
+  document.getElementById('subBinary').textContent = diffBin.join('');
+  multBin(); // Multiply Binary
+}
+
+let subOperation = (arr1, arr2) => {
+  let arr = [];
+  for(let i = arr1.length - 1; i >= 0; i--) {
+    let diff = Number(arr1[i])-Number(arr2[i]);
+    if(diff == -1) {arr1[i-1] = Number(arr1[i-1]-1);diff = 1;}
+    if(diff == -2) {arr1[i-1] = Number(arr1[i-1]-1);diff = 0;}
+    arr.unshift(diff);
+  }
+  return arr;
+}
+
+let multBin = () => {
+  let bin1 = baseBinary1.textContent.replace(/^0+/,'');
+  let bin2 = baseBinary2.textContent.replace(/^0+/,'');
+}
+
+let divBin = () => {
 
 }
 
-let addDec = () => {
-
+let checkHigher = (arr1, arr2) => {
+  for (let i in arr1) {
+    if(arr1[i] > arr2[i]) return 'arr1';
+    else if(arr1[i] < arr2[i]) return 'arr2';
+  }
+  return 'equal';
 }
 
 let toggle = (baseNum) => {
@@ -53,5 +130,19 @@ let toggle = (baseNum) => {
   else
     baseValue.innerText = 0;
   concatBin();
-  bin2Dec();
 }
+
+let changeAll = (num) => {
+  let but1 = document.querySelectorAll('.binaryInput1 button');
+  for(let i = 0; i < but1.length; i++) {
+    but1[i].textContent = num;
+  }
+  let but2 = document.querySelectorAll('.binaryInput2 button');
+  for(let i = 0; i < but2.length; i++) {
+    but2[i].textContent = num;
+  }
+  concatBin();
+}
+
+window.onload = concatBin();
+window.onload = bin2Dec();
